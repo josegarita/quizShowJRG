53615d15235b314a9832156261d8c5f11c992c21
Ubuntu <ubuntu@ip-172-31-52-149.ec2.internal>
2015-03-18 16:38:13 +0000

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express')
+var express = require(’express’) 
+var fs = require(’fs’)
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var quiz = JSON.parse(fs.readFileSync(’quiz.json’));
+app.set(’port’, (process.env.PORT || 8080)) /* used to be 5000 */ 
+app.use(express.static(__dirname + ’/public’))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get(’/’, function(request, response) { 
+buffer = new Buffer(fs.readFileSync(’index.html’)) 
+response.send(buffer.toString(’utf8’)) 
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get(’port’), function() { 
+console.log("Node app is running at localhost:" + app.get(’port’)) 
 })
